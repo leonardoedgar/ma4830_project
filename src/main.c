00336b78e0cb592ca899a9984e49eb7dd76fae98
leonardoedgar <leonardo_edgar98@outlook.com>
2020-10-01 16:15:23 +0800

    implement a function to print collision-free traj in a table and bugfix in overflow char for requesting char from user

--- src/main.c
+++ src/main.c
@@ -9,7 +9,7 @@ int main() {
     double distance;
     LaunchConfig launchConfigArr[LAUNCH_CONFIG_ARR_SIZE] = {};
     int validProjectileVelocities[] = {110, 115, 120, 125, 130}, index;
-    print_launch_velocities_table(validProjectileVelocities, 5, 45.0, 5.0, 85.0);
+    print_augmentation_table(validProjectileVelocities, 5, 45.0, 5.0, 85.0);
     distance = get_user_input(&terrain);
     for (index=0; index<5; index++) {
         LaunchConfig launch;
@@ -21,19 +21,7 @@ int main() {
     if (status.success) {
         status = get_collision_free_trajectories(launchConfigArr, LAUNCH_CONFIG_ARR_SIZE, &terrain);
         if (status.success) {
-            if (fabs(launchConfigArr[0].angle) < FLOATING_POINT_PRECISION && launchConfigArr[0].velocity == 0) {
-                printf("There is no any feasible configurations to hit the target.\n");
-            }
-            else {
-                printf("Here are all possible configurations to hit the target.\n");
-                for (index=0; index<LAUNCH_CONFIG_ARR_SIZE; index++) {
-                    if (fabs(launchConfigArr[index].angle) < FLOATING_POINT_PRECISION && launchConfigArr[index].velocity == 0) {
-                        break;
-                    }
-                    printf("velocity: %d m/s\t angle: %.2f deg\n", launchConfigArr[index].velocity,
-                           launchConfigArr[index].angle);
-                }
-            }
+            print_launch_configuration_table(launchConfigArr, LAUNCH_CONFIG_ARR_SIZE);
         }
         else {
             printf("%s", status.errorMessage);
--- src/user_interface.c
+++ src/user_interface.c
@@ -23,10 +23,10 @@ double get_positive_float_from_user_input(char* message) {
         getchar();
         number = strtod(userInput, &leftoverBuffer);
         if (fabs(number) < FLOATING_POINT_PRECISION || *leftoverBuffer) {
-            printf("Input is not a number. Retry.\n");
+            printf("Input is not a number. Retry.\n\n");
         }
         else if (number < 0) {
-            printf("Input is not a positive number. Retry.\n");
+            printf("Input is not a positive number. Retry.\n\n");
         }
         else {
             isUserInputValid=true;
@@ -44,12 +44,17 @@ double get_positive_float_from_user_input(char* message) {
 double get_user_input(ObstacleInfo* obstacle) {
     double distance;
     char isTerrainExistChar;
+    char leftoverBuffer;
     bool isTerrainExistBool, isUserInputValid=false;
     distance = get_positive_float_from_user_input("Enter target distance from mortar: ");
     while (!isUserInputValid) {
         printf("Is there any obstacles (y/n)? ");
         scanf("%c", &isTerrainExistChar);
-        while ((getchar()) != '\n');
+        if (getchar() != '\n') {
+            while(getchar() != '\n');
+            printf("Input is invalid. Please only enter (y/n). Retry.\n\n");
+            continue;
+        };
         switch (tolower(isTerrainExistChar)) {
             case 'y':
                 isTerrainExistBool = true;
@@ -60,7 +65,7 @@ double get_user_input(ObstacleInfo* obstacle) {
                 isUserInputValid = true;
                 break;
             default:
-                printf("Input is invalid. Please only enter (y/n). Retry.\n");
+                printf("Input is invalid. Please only enter (y/n). Retry.\n\n");
         }
     }
     if (isTerrainExistBool) {
@@ -71,14 +76,15 @@ double get_user_input(ObstacleInfo* obstacle) {
 }
 
 /**
- * A function to print a table that contains the maximum range and height given an array of velocities of the trajectory.
+ * A function to print an augmentation table that contains the maximum speed and height given an array of
+ * velocity of the trajectory.
  * @param launchVelocityArr contains launch velocities of the trajectory.
  * @param arraySize indicates the launch velocity array size.
  * @param startAngle indicates the projectile launch start angle.
  * @param stepAngle indicates the projectile launch step angle.
  * @param endAngle indicates the projectile launch end angle.
  */
-void print_launch_velocities_table(const int* launchVelocityArr, int arraySize, double startAngle, double stepAngle,
+void print_augmentation_table(const int* launchVelocityArr, int arraySize, double startAngle, double stepAngle,
         double endAngle) {
     double angle, maxRange, maxHeight;
     int index, augmentationNumber;
@@ -86,16 +92,15 @@ void print_launch_velocities_table(const int* launchVelocityArr, int arraySize, 
     int numSpaceBetweenTitle = (29*arraySize-(int)strlen(tableTitle)+10)/2;
     LaunchConfig launch;
     FunctionStatus status;
-    printf("The table of the maximum range and height for different muzzle velocities are listed as below.\n");
-    printf("\f");
+    printf("The table of the maximum range and height for different muzzle velocities are listed as below.\n\f");
     printf("%*c", numSpaceBetweenTitle, ' ');
     printf("Augmentation Table");
     printf("%*c\n", numSpaceBetweenTitle, ' ');
-    printf("==========");
+    printf("=============");
     for (index=0; index<arraySize; index++) {
         printf("=============================");
     }
-    printf("\n|        |");
+    printf("\n|           |");
     for (index=0; index<arraySize; index++) {
         launch.velocity = launchVelocityArr[index];
         augmentationNumber = get_augmentation_number(&launch, &status);
@@ -106,22 +111,22 @@ void print_launch_velocities_table(const int* launchVelocityArr, int arraySize, 
             printf("       Augmentation %d       |", augmentationNumber);
         }
     }
-    printf("\n| Degree |");
+    printf("\n| Angle (°) |");
     for (index=0; index<arraySize; index++) {
         printf("----------------------------|");
     }
-    printf("\n|        |");
+    printf("\n|           |");
     for (index=0; index<arraySize; index++) {
         printf(" range (m) | max height (m) |");
     }
-    printf("\n|--------|");
+    printf("\n|-----------|");
     for (index=0; index<arraySize; index++) {
         printf("----------------------------|");
     }
     printf("\n");
     for (angle = startAngle; angle < endAngle; angle += stepAngle)
     {
-        printf("|%7.2f |", angle);
+        printf("| %7.2f   |", angle);
         for (index=0; index<arraySize; index++)
         {
             launch.velocity = launchVelocityArr[index];
@@ -133,9 +138,44 @@ void print_launch_velocities_table(const int* launchVelocityArr, int arraySize, 
 
         printf("\n");
     }
-    printf("==========");
+    printf("=============");
     for (index=0; index<arraySize; index++) {
         printf("=============================");
     }
     printf("\n");
+}
+
+/**
+ * A function to print a table containing of different mortar's launch configurations.
+ * @param launchConfigArr contains different kinds of launch configuration of the mortar.
+ * @param arraySize indicates the launch configuration array size.
+ */
+void print_launch_configuration_table(const LaunchConfig* launchConfigArr, int arraySize) {
+    int index, augmentationNumber;
+    FunctionStatus status;
+    if (fabs(launchConfigArr[0].angle) < FLOATING_POINT_PRECISION && launchConfigArr[0].velocity == 0) {
+        printf("There is no any feasible configurations to hit the target.\n");
+    }
+    else {
+        printf("Here are all possible configurations to hit the target.\n\f");
+        printf("             Launch Configuration Table              \n");
+        printf("=====================================================\n");
+        printf("|  Augmentation Number | Velocity (m/s) | Angle (°) |\n");
+        printf("|---------------------------------------------------|\n");
+        for (index=0; index<arraySize; index++) {
+            if (fabs(launchConfigArr[index].angle) < FLOATING_POINT_PRECISION && launchConfigArr[index].velocity == 0) {
+                break;
+            }
+            augmentationNumber = get_augmentation_number(&launchConfigArr[index], &status);
+            if (!status.success) {
+                printf("|           ?          |       %d      |   %.2f   |\n",
+                       launchConfigArr[index].velocity, launchConfigArr[index].angle);
+            }
+            else {
+                printf("|           %d          |       %d      |   %.2f   |\n",
+                       augmentationNumber, launchConfigArr[index].velocity, launchConfigArr[index].angle);
+            }
+        }
+        printf("=====================================================\n");
+    }
 }
